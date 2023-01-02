7244b142341b7fec8e4165509da466258305c882
Damaj301damaj-lol <mohamad301Damaj@duck.com>
2023-01-02 19:45:16 +0000

    Release v1.0!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # termux-update-nsp
+This is a script to update your NSP switch games based of the other [script](https://gist.github.com/Vixeliz/62bdbfa081a7716c3b71b30c101f2912) but with a bunch of improvements!
+
+This work is a part of Public Domain
--- termux_update_nsp.sh
+++ termux_update_nsp.sh
@@ -2,36 +2,39 @@
 
 # Updated Bash Script to fix a bunch of issues like no error checking
 
-# Check if apt or pacman is used, then update the system, and exit if update failed (exit code = 1)
+# Check if apt or pacman is used, then update the system, then install dependencies and exit if update failed (exit code = 1)
 case $TERMUX_APP_PACKAGE_MANAGER in
-	apt) apt update && apt upgrade || exit 1;;
-	pacman) pacman -Syu || exit 1;;
-	*) pkg upgrade || exit 1;;
+	apt) apt update && apt upgrade && apt install cmake make clang binutils || echo "Failed!, don't decline updates/installations!" && exit 1;;
+	pacman) pacman -Syu && pacman -S cmake make clang binutils || echo "Failed!, don't decline updates/installations!" && exit 1;;
+	*) pkg upgrade && pkg install cmake make clang binutils || echo "Failed!, don't decline updates/installations!" && exit 1;;
 esac
 
-pkg i bash cmake make vim clang git binutils
+# Making a temp dir to build packages
+mkdir temp && echo "make sure you are in the git repo!" || continue
 
-mkdir SwitchUpdatePatcher
-cd SwitchUpdatePatcher
+# Installing the dependencies and compiling them!
 
-curl -o nsp_update_patcher.sh https://gist.githubusercontent.com/Vixeliz/eb2f038d59beafff14e49b56c14e469c/raw/a88f012227fb703e5b0a009d01a6f7670f5ba3e4/nsp_update_patcher.sh
-chmod +x nsp_update_patcher.sh
-
-git clone https://github.com/SciresM/hactool
-mv hactool hactoolsrc
-cd hactoolsrc
-git checkout c2c907430e674614223959f0377f5e71f9e44a4a
-mv config.mk.template config.mk
-sed -i '372d' main.c
-mv main.temp main.c
-make
+# hactool
+cd hactool
+make || echo "Compilation Failed!, please report this bug at https://github.com/Damaj301damaj-lol/termux-update-nsp" && exit 1
 mv hactool ..
 cd ..
-
-git clone https://github.com/The-4n/hacPack
+# hacpack
 cd hacPack
-git checkout 7845e7be8d03a263c33430f9e8c2512f7c280c88
-mv config.mk.template config.mk
-make
+make || echo "Compilation Failed!, please report this bug at https://github.com/Damaj301damaj-lol/termux-update-nsp" && exit 1
 mv hacpack ..
 cd ..
+
+# now exit the temp folder
+cd ..
+
+# curl the script to save it
+curl -o https://gist.github.com/Vixeliz/eb2f038d59beafff14e49b56c14e469c/raw/a88f012227fb703e5b0a009d01a6f7670f5ba3e4/nsp_update_patcher.sh || echo "Make sure you are online!" && exit 1
+# chmod +x it
+chmod +x nsp_update_patcher.sh
+
+# print our status!
+echo "We are Done!, do NOT remove anything from this dir, now, you can patch your games with ./nsp_update_patcher.sh BASE UPDATE"
+
+# exit with a good exit code
+exit 0
